5c3a31e0e69620d2b99607e9808a00b423b30570
zezking <zhaoenze001@gmail.com>
2021-02-12 20:09:21 +0000

    create transaction super class

--- index.js
+++ index.js
@@ -1,39 +1,50 @@
 let balance = 500.0;
 
-class Withdrawal {
-  constructor(amount) {
-    this.amount = amount;
-  }
-
-  commit() {
-    balance -= this.amount;
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
   }
 }
 
-class Deposit {
-  constructor(amount) {
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
-
+}
+class Withdrawal extends Transaction {
   commit() {
-    balance += this.amount;
+    this.account.balance -= this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  commit() {
+    this.account.balance += this.amount;
   }
 }
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const Account1 = new Account("Enze");
+
+t1 = new Withdrawal(100, Account1);
 t1.commit();
+console.log(t1);
+// console.log(Account1);
+// t1 = new Withdrawal(50.25);
+// t1.commit();
 
-console.log("Transaction 1:", t1);
+// console.log("Transaction 1:", t1);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log("Transaction 2:", t2);
+// t2 = new Withdrawal(9.99);
+// t2.commit();
+// console.log("Transaction 2:", t2);
 
-t3 = new Deposit(100);
-t3.commit();
-console.log("Transaction 3", t3);
+// t3 = new Deposit(100);
+// t3.commit();
+// console.log("Transaction 3", t3);
 
-console.log("Balance:", balance);
+// console.log("Balance:", balance);
